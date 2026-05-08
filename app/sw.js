90c6ce8cbce299885e1120940484855afb8f0b45
cmfilings-git <cmfilings.in@gmail.com>
2026-05-09 01:37:16 +0530

    Create sw.js

---
+++ app/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'cmfilings-cache-v1';
+
+const urlsToCache = [
+    '/app/',
+    '/app/index.html',
+    '/app/css/style.css',
+    '/app/js/app.js',
+    '/app/js/theme.js',
+    '/app/assets/icon-app.png'
+];
+
+self.addEventListener('install', event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+
+});
+
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+
+});
