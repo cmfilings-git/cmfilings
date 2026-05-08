8f5e387f04a4703f13746fb52e9bbdebde4d1081
cmfilings-git <cmfilings.in@gmail.com>
2026-05-09 01:34:15 +0530

    Create js/theme.js

---
+++ app/js/theme.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById('theme-toggle');
+
+const savedTheme = localStorage.getItem('theme');
+
+if(savedTheme === 'dark'){
+    document.body.classList.add('dark-mode');
+}
+
+toggleButton.addEventListener('click', () => {
+
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        localStorage.setItem('theme', 'dark');
+    }else{
+        localStorage.setItem('theme', 'light');
+    }
+
+});
