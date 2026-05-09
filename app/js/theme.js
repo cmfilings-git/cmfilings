e94570877d03de945e88edd660f019c72d5f7b90
cmfilings-git <cmfilings.in@gmail.com>
2026-05-09 16:43:44 +0530

    Update theme.js

--- app/js/theme.js
+++ app/js/theme.js
@@ -1,19 +1,23 @@
-const toggleButton = document.getElementById('theme-toggle');
+const moonButton = document.querySelector('.icon-moon');
 
-const savedTheme = localStorage.getItem('theme');
-
-if(savedTheme === 'dark'){
-    document.body.classList.add('dark-mode');
-}
-
-toggleButton.addEventListener('click', () => {
+moonButton.addEventListener('click', () => {
 
     document.body.classList.toggle('dark-mode');
 
     if(document.body.classList.contains('dark-mode')){
-        localStorage.setItem('theme', 'dark');
+        localStorage.setItem('theme','dark');
     }else{
-        localStorage.setItem('theme', 'light');
+        localStorage.setItem('theme','light');
+    }
+
+});
+
+window.addEventListener('load', () => {
+
+    const savedTheme = localStorage.getItem('theme');
+
+    if(savedTheme === 'dark'){
+        document.body.classList.add('dark-mode');
     }
 
 });
